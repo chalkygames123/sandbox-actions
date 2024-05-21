079a0d9f8f32ad3b6614543c26b2a8111ba85172
Takuya Fukuju <chalkygames123@gmail.com>
2024-05-21 16:59:32 +0900

    Update .github/actions/build/action.yaml, .github/actions/check/action.yaml, .github/actions/deploy/action.yaml, .github/workflows/ci-cd-admin.yaml, .github/workflows/ci-cd-web.yaml, .prettierignore, apps/admin/package.json, apps/admin/src/index.html, apps/web/package.json, apps/web/src/index.html, package.json, pnpm-lock.yaml and prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+export default {
+	plugins: ['prettier-plugin-packagejson'],
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+};
